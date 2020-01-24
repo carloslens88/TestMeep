21ff6dcfc97681c9eb591cef63a812bdfb973418
Carlos Lens <carloslens88@gmail.com>
2020-01-24 15:35:40 +0100

    First Commit

---
+++ src/test/java/com/example/meep/MeepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
